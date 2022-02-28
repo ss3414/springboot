ac1acbb5313befcea5a030c59be53309d10ec3e4
ss3414 <3414523068@qq.com>
2022-02-28 14:55:25 +0800

    220228

--- springboot_swagger/src/main/java/com/controller/IndexController.java
+++ springboot_swagger/src/main/java/com/controller/IndexController.java
@@ -5,13 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController /* 取代@Controller，默认返回JSON，不需要@ResponseBody */
+@RestController
 @RequestMapping("")
 public class IndexController {
 
-    /**
-     * @title 首页
-     */
     @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("/index");
